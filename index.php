d4849b5318ccef932a1dc72845fbb809d3d1f853
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-03 13:02:10 +0200

    Esimene commit, 03.01.2018, index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// Tekstimuutuja
+$tervitus = 'Hello';
+# Täisarvuline muutuja
+$nr = 1;
+// Kümnendmurd
+$nr = 10.0121;
+// Tõeväärtusmuutuja
+$bool = False;
+/*
+Massiiv (php ja JS - array, python - list)
+Üherealine kommentaar on // ja #
+*/
+
+$massiiv = array('Nimi',1974,10.81, 'Perenimi');
+
+echo "$tervitus world!<br>";
+echo '$tervitus world!<br>';
+echo $massiiv[0];
+
+
+
+?>
