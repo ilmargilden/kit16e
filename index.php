23f2e8e071534002c8844387cb2b5b96e951778f
ilmargilden <32132348+ilmargilden@users.noreply.github.com>
2018-01-25 08:37:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php
+<meta charset="UTF-8" />
 
+<?php
 // Tekstimuutuja
 $tervitus = 'Hello';
-# Täisarvuline muutuja
+# Täisar4vuline muutuja
 $nr = 1;
 // Kümnendmurd
 $nr = 10.0121;
@@ -12,18 +13,45 @@ $bool = False;
 Massiiv (php ja JS - array, python - list)
 Üherealine kommentaar on // ja #
 */
-
-$massiiv = array('Nimi',1974,10.81, 'Perenimi');
-
+$massiiv = array('Nimi',1974,10.81,'Perenimi' );
+$esta = 'Eesti keel';
+$mat = 'Metemaatika';
+$fyss = 'Füüsika';
+// sidusmassiivid
+$hinded = array('Peeter'=>array($esta=>4, $mat=>4, $fyss=>3),
+                'Mari'=>array($esta=>5, $mat=>4, $fyss=>3),
+                'Jüri'=>array($esta=>4, $mat=>4, $fyss=>3));
 echo "$tervitus world!<br>";
 echo '$tervitus world!<br>';
-echo $massiiv[0];
-
-if($bool == False){
-
-  print_r ($massiiv);
-} else {echo "Tõeväärtus ei vasta nõutule"; }
-
-
-
-?>
+// echo $massiiv[0].'<br>';
+// Tingimuslause
+if ($bool == False){
+// massiivi xisu näitamine
+//  print_r($massiiv);
+for ($i = 0; $i < count($massiiv); $i++ ) {
+  echo $massiiv[$i].'<br>';
+}
+} else { echo "Tõeväärtus ei vasta nõutule"; }
+$hinded_id = array_keys($hinded);
+for ($i=0; $i < count($hinded); $i++ ){
+  echo $hinded_id[$i].'<br>';
+  foreach ($hinded[$hinded_id[$i]] as $aine => $hinne) {
+    echo $aine." : ".$hinne.'<br>';
+    # code...
+  }
+}
+$file = fopen('counter.txt', "w") or die("Ei saanud faili avada");
+if (!$file){
+$txt = 1;
+fwrite($file,$txt);
+fclose($file);
+} else {
+  $txt = fread($file, filesize('counter.txt'));
+  $new_txt = $txt + 1;
+  fwrite($file,$new_txt);
+  fclose($file);
+}
+$file = fopen('counter.txt', "r") or die("Ei saanud faili avada");
+echo fread($file, filesize('counter.txt'));
+fclose($file);
+ ?>
